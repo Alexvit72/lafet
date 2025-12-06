ff698f1879db26fbcb6727693d94ebaac7ef6682
Александр <aleksandrvitebsk@tut.by>
2025-12-06 14:01:10 +0300

    fix

--- data.ts
+++ data.ts
@@ -30,21 +30,21 @@ export const trailers: Array<Item> = [
 export const lafets: Array<Item> = [
   {
     id: "1",
-    image: "/item_1.png",
+    image: item_1,
     title: "Лафет 1",
     description: "Очень хороший",
     price: "20 руб / сутки",
   },
   {
     id: "2",
-    image: "/item_2.png",
+    image: item_2,
     title: "Лафет 2",
     description: "Супер пупер",
     price: "25 руб / сутки",
   },
   {
     id: "3",
-    image: "/item_3.png",
+    image: item_3,
     title: "Лафет 3",
     description: "Офигительный",
     price: "30 руб / сутки",
