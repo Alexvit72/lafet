31da9458b31742db1b3cf2125361b200ebe70e23
Александр <aleksandrvitebsk@tut.by>
2025-12-06 13:19:40 +0300

    last try deploy

--- components/home/Catalog.tsx
+++ components/home/Catalog.tsx
@@ -8,7 +8,7 @@ export default function Catalog() {
   return (
     <div className='p-8 sm:p-16 mx-auto w-min-xs sm:w-min-sm'>
       <h2 className='mb-8 sm:mb-16 text-center text-4xl sm:text-6xl font-bold text-orange-700'>
-        Каталог
+        Каталог новый
       </h2>
       <div className='flex flex-wrap justify-center sm:justify-between'>
         {items.map((item, index) => (
