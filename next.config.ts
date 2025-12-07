f906cadcd812c6c7f3264e3b5171efb3e0b305ac
Александр <aleksandrvitebsk@tut.by>
2025-12-07 14:06:18 +0300

    update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/lafet",
-  assetPrefix: "/lafet/",
+  assetPrefix: "/lafet",
   images: { unoptimized: true },
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
